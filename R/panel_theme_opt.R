ac77c9c6573fa5f8e2f23449d5a0efefec594434
Alba Maiques <maiques@hotmail.fr>
2024-10-23 19:00:02 +0200

    css param bar

--- R/panel_theme_opt.R
+++ R/panel_theme_opt.R
@@ -21,6 +21,26 @@ panel_theme_opt <- function(id){
         selected = "minimal"
       ),
 
+      actionButton("action", label = "Action") ,
+      checkboxInput("checkbox", label = "Choice A", value = TRUE),
+      checkboxGroupInput("checkGroup", label = h3("Checkbox group"),
+                         choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                         selected = 1),
+      dateRangeInput("dates", label = h3("Date range")),
+      hr(),
+      numericInput("num", label = h3("Numeric input"), value = 1),
+      radioButtons("radio", label = h3("Radio buttons"),
+                   choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                   selected = 1),
+      selectInput("select", label = h3("Select box"),
+                  choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                  selected = 1),
+      hr(),
+      sliderInput("slider2", label = h3("Slider Range"), min = 0,
+                  max = 100, value = c(40, 60)),
+      textInput("text", label = h3("Text input"), value = "Enter text..."),
+      fileInput("file", label = h3("File input")),
+
       h4("Scales")
   )
 }
